a1bc5b466bfcb41b4f6e8537d75f70cff8a38f57
start.vaadin.com <no-reply@vaadin.com>
2022-11-05 23:19:59 +0000

    Generated project

---
+++ src/main/java/com/learn/support/data/service/SamplePersonRepository.java
@@ -0,0 +1,9 @@
+package com.learn.support.data.service;
+
+import com.learn.support.data.entity.SamplePerson;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
+
+}
